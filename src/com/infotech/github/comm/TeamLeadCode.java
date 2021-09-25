bbf54756d7a35fd514eee484ffa2797983cb80eb
Chandu Velagapudi <chandu.velagapudi22@gmail.com>
2021-09-25 13:06:53 +0530

    This ismy frist commit i am team lead

---
+++ src/com/infotech/github/comm/TeamLeadCode.java
@@ -0,0 +1,8 @@
+package com.infotech.github.comm;
+
+public class TeamLeadCode {
+	public static void main(String[] args) {
+		System.out.println("Hi  iam the team lead");
+		System.out.println("I DEVELOPED THE CODE ");
+	}
+}
